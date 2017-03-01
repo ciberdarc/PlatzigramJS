ba65c10987e3c245986289bb4d7747f4f9168518
Alexis Araujo <ala1289@gmail.com>
2017-03-01 11:20:17 -0600

    Add gulp and sass

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
-  res.send('hello world')
+  res.render('index')
 })
 
 app.listen(3000, function(err){
-	if(err) return console.log('Hubo un error'), process.exit(1);
-	console.log('Escuchando el puerto 3000');
+    if(err) return console.log('Hubo un error'), process.exit(1);
+    console.log('Escuchando el puerto 3000');
 })
