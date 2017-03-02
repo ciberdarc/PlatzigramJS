2b3a743cad51e730188a789f9e839e035a88cf99
Alexis Araujo <ala1289@gmail.com>
2017-03-02 13:49:49 -0600

    Add browserify and babel and EMSC2015 and page.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
+var babel = require('babelify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var preset = require('babel-preset-es2015');
+
+// Require = Buscar dentro de node modules el directorio llamado y va a devolver lo que nos exporte la herramienta
 
 gulp.task('styles',function(){
     gulp
@@ -15,4 +21,16 @@ gulp.task('assets', function(){
       .pipe(gulp.dest('public'));
 })
 
-gulp.task('default', ['styles', 'assets'])
+gulp.task('scripts',function(){
+	browserify('./src/index.js')
+		.transform(babel, preset)
+		.bundle()
+		.pipe(source('index.js'))
+		.pipe(rename('app.js'))
+		.pipe(gulp.dest('public'));
+
+})
+
+// vinyl-source-stream nos permite pasar de browserify a gulp para que gulp si pueda seguir procesando el archivo
+
+gulp.task('default', ['styles', 'assets', 'scripts'])
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.set('view engine', 'pug');
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
+app.get(['/','/signup','signin'], function (req, res) {
   res.render('index')
 })
 
