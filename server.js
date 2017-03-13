f113e170c9b159c158a5b3c31087b83c400ff4aa
Alexis Araujo <ala1289@gmail.com>
2017-03-13 12:52:10 -0600

    Add SuperAgent

--- server.js
+++ server.js
@@ -1,16 +1,44 @@
-var express = require('express');
+var express = require('express')
 
-var app = express();
+var app = express()
 
-app.set('view engine', 'pug');
+app.set('view engine', 'pug')
 
-app.use(express.static('public'));
+app.use(express.static('public'))
 
-app.get(['/','/signin','/signup'], function (req, res) {
-  res.render('index');
+app.get(['/', '/signin', '/signup'], function (req, res) {
+  res.render('index')
 })
 
-app.listen(3000, function(err){
-    if(err) return console.log('Hubo un error'), process.exit(1);
-    console.log('Escuchando el puerto 3000');
+app.get('/api/pictures', function (req, res) {
+  var pictures = [
+      {
+    		user: {
+    				username: 'alexisaraujo',
+    				avatar: 'https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/15095636_10155681383018539_239145637589870926_n.jpg?oh=a116b9c31e753fd9cbea87d27e7eef57&oe=593604FA'
+    		},
+    		url: 'office.jpg',
+    		likes: 0,
+    		liked: false,
+            createdAt: new Date().getTime()
+    	},
+    	    	{
+    		user: {
+    				username: 'alexisaraujo',
+    				avatar: 'https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/15095636_10155681383018539_239145637589870926_n.jpg?oh=a116b9c31e753fd9cbea87d27e7eef57&oe=593604FA'
+    		},
+    		url: 'office.jpg',
+    		likes: 2,
+    		liked: true,
+            createdAt: new Date().setDate(new Date().getDate() - 10)
+    }
+  ]
+  setTimeout(function () {
+    res.send(pictures)
+  }, 2000)
+})
+
+app.listen(3000, function (err) {
+  if (err) return console.log('Hubo un error'), process.exit(1)
+  console.log('Escuchando el puerto 3000')
 })
--- src/homepage/index.js
+++ src/homepage/index.js
@@ -1,34 +1,23 @@
-var page = require('page');
-var empty = require('empty-element');
-var template = require('./template');
-var title =require('title');
+var page = require('page')
+var empty = require('empty-element')
+var template = require('./template')
+var title = require('title')
+var request = require('superagent')
 
-page('/', function(ctx, next){
-    title('Platzigram')
-    var main = document.getElementById('main-container');
+page('/', loadPictures, function (ctx, next) {
+  title('Platzigram')
+  var main = document.getElementById('main-container')
 
-    var pictures = [
-    	{
-    		user:{
-    				username: 'alexisaraujo',
-    				avatar: 'https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/15095636_10155681383018539_239145637589870926_n.jpg?oh=a116b9c31e753fd9cbea87d27e7eef57&oe=593604FA'
-    		},
-    		url: 'office.jpg',
-    		likes: 0,
-    		liked: false,
-            createdAt: new Date()
-    	},
-    	    	{
-    		user:{
-    				username: 'alexisaraujo',
-    				avatar: 'https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/15095636_10155681383018539_239145637589870926_n.jpg?oh=a116b9c31e753fd9cbea87d27e7eef57&oe=593604FA'
-    		},
-    		url: 'office.jpg',
-    		likes: 2,
-    		liked: true,
-            createdAt: new Date().setDate(new Date().getDate() - 10)
-    	}
-    ];
-
-    empty(main).appendChild(template(pictures));
+  empty(main).appendChild(template(ctx.pictures))
 })
+
+function loadPictures (ctx, next) {
+  request
+    .get('/api/pictures')
+    .end(function (err, res) {
+      if (err) return console.log(err)
+
+      ctx.pictures = res.body
+      next()
+    })
+}
