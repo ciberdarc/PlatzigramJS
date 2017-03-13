bd414fa0b31c9f86a1278517efecec5186860952
Alexis Araujo <ala1289@gmail.com>
2017-03-13 11:05:31 -0600

    Change languaje spanish or english

--- src/translate/en-US.js
+++ src/translate/en-US.js
@@ -1,6 +1,21 @@
 module.exports = {
-	likes:   '{likes, plural, ' +
+  likes:   '{likes, plural, ' +
             '=0 { no likes}' +
             '=1 {# like}' +
-            'other {# likes}}'
+            'other {# likes}}',
+  'logout': 'Logout',
+  'english': 'English',
+  'spanish': 'Spanish',
+  'signup.subheading': 'Sgnup to watch your friends\' pictures studying at Platzi',
+  'signup.facebook': 'Singup with Facebook',
+  'signup.text': 'Signup',
+  'email': 'Email',
+  'username': 'Username',
+  'fullname': 'Fullname',
+  'password': 'Password',
+  'signup.call-to-action': 'Signup',
+  'signup.have-account': 'Already have an account?',
+  'signin': 'Signing',
+  'signin.not-have-account': '¿Dont\'t have an account',
+  'languaje': 'Languaje'
 }
--- src/translate/es.js
+++ src/translate/es.js
@@ -1,3 +1,18 @@
 module.exports = {
-	likes:  '{	likes , number } me gusta'
+	'likes':  '{	likes , number } me gusta',
+	'logout': 'Salir',
+	'english': 'Ingles',
+	'spanish': 'Español',
+	'signup.subheading': 'Registrate para ver fotos de tus amigos estudiando en Platzi',
+	'signup.facebook': 'Iniciar sesion con Facebook',
+	'signup.text': 'Iniciar Sesion',
+	'email': 'Correo electronico',
+	'username': 'Nombre de usuario',
+	'fullname': 'Nombre completo',
+	'password': 'Contraseña',
+	'signup.call-to-action': 'Registrate',
+	'signup.have-account': '¿Tienes una cuenta?',
+	'signin': 'Entrar',
+	'signin.not-have-account': '¿No tienes una cuenta?',
+	'languaje': 'Idioma'
 }
