4206b86756dd750a0812d28461ef6da05010834f
Alexis Araujo <ala1289@gmail.com>
2017-03-13 14:32:35 -0600

    Add FetchAPI

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -2,36 +2,49 @@ var page = require('page')
 var empty = require('empty-element')
 var template = require('./template')
 var title = require('title')
-var request = require('superagent')
+// var request = require('superagent')
 var header = require('../header')
-var axios = require('axios')
+// var axios = require('axios')
 
-page('/', header, loadPicturesAxios, function (ctx, next) {
+page('/', header, loadPicturesFetch, function (ctx, next) {
   title('Platzigram')
   var main = document.getElementById('main-container')
 
   empty(main).appendChild(template(ctx.pictures))
 })
 
-function loadPictures (ctx, next) {
-  request
-    .get('/api/pictures')
-    .end(function (err, res) {
-      if (err) return console.log(err)
+// function loadPictures (ctx, next) {
+//   request
+//     .get('/api/pictures')
+//     .end(function (err, res) {
+//     if (err) return console.log (err)
+//       ctx.pictures = res.body
+//       next()
+//     })
+// }
 
-      ctx.pictures = res.body
-      next()
-    })
-}
+// function loadPicturesAxios (ctx, next) {
+//   axios
+//     .get('/api/pictures')
+//     .then(function (res) {
+//       ctx.pictures = res.data
+//       next()
+//     })
+//     .catch(function (err) {
+//       console.log(err)
+//     })
+// }
 
-function loadPicturesAxios (ctx, next) {
-  axios
-    .get('/api/pictures')
-    .then(function (res) {
-      ctx.pictures = res.data
-      next()
-    })
-    .catch(function (err) {
-      console.log(err);
-    })
+function loadPicturesFetch (ctx, next) {
+  fetch('/api/pictures')
+  .then(function (res) {
+    return res.json()
+  })
+  .then(function (pictures) {
+    ctx.pictures = pictures
+    next()
+  })
+  .catch(function (err) {
+    console.log(err)
+  })
 }
