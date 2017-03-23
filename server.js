adf7cb2ab603d3aea8c54fe501a529e1bed11be3
Alexis Araujo <ala1289@gmail.com>
2017-03-23 14:24:46 -0600

    Config AWS SDK and Multer S3

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 var express = require('express')
 var multer = require('multer')
 var ext = require('file-extension')
+var aws = require('aws-sdk')
+var multerS3 = require('multer-s3')
 
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads')
+var config = require('./config')
+
+var s3 = new aws.S3({
+  accessKeyId: config.aws.accessKey,
+  secretAccessKey: config.aws.secretKey
+})
+
+var storage = multerS3({
+  s3: s3,
+  bucket: 'alexisgram',
+  acl: 'public-read',
+  metadata: function (req, file, cb) {
+    cb(null, {fieldName: file.fieldname})
   },
-  filename: function (req, file, cb) {
+  key: function (req, file, cb) {
     cb(null, +Date.now() + '-' + ext(file.originalname))
   }
 })
